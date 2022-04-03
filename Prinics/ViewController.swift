ce5ae56f92d17dd78d95aba45f6f09b299772b02
Sungho Lee <sh1217sh@gmail.com>
2022-04-02 18:09:45 -0700

    Xcode project created

---
+++ Prinics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prinics
+//
+//  Created by Sungho Lee on 4/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
